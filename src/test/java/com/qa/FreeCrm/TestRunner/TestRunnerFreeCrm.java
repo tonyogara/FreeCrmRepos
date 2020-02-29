45f2949c514dc1c7def5fae9bf302afd79cc10d5
Tony <tony@Pauls-MacBook-Pro.local>
2020-02-29 10:35:14 +0000

    updated runner details

--- src/test/java/com/qa/FreeCrm/TestRunner/TestRunnerFreeCrm.java
+++ src/test/java/com/qa/FreeCrm/TestRunner/TestRunnerFreeCrm.java
@@ -3,9 +3,17 @@ package com.qa.FreeCrm.TestRunner;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
+import java.io.*;
+import org.junit.AfterClass;
+import org.junit.runner.RunWith;
+import com.cucumber.listener.Reporter;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
 
-//@RunWith(Cucumber.class)
-@CucumberOptions(features="src/main/resources/Features1", glue="com.qa.FreeCrm.StepSpecifications1", tags = {"@SmokeTest"},
+
+@RunWith(Cucumber.class)
+//@CucumberOptions(features="src/main/resources/Features/ContactCreation.feature", glue="StepSpecs", 
+@CucumberOptions(features="src/main/resources/Features/ContactCreation.feature", glue="com.qa.FreeCrm.StepSpecifications",
 							plugin = {"pretty", "html:target/cucumber"})
 
 //Plugin={“com.cucumber.listener.ExtentCucumberFormatter:target/html/ExtentReport.html“})
@@ -14,20 +22,7 @@ import cucumber.api.CucumberOptions;
 
 public class TestRunnerFreeCrm 
 {
-/*
-	@AfterClassA
-	public static void setup()
-	{
-		Reporter.loadXMLConfig(new File("src/main/resources/extent-config.xml"));
-		//Reporter.loadXMLConfig(new File("src/test/resources/extent-config.xml"));
-		//Reporter.setSystemInfo("Test User", System.getProperty("user.name"));
-		Reporter.setSystemInfo("User Name", "Tony");
-		Reporter.setSystemInfo("Application Name", "Test App ");
-		Reporter.setSystemInfo("Operating System Type", System.getProperty("os.name").toString());
-		Reporter.setSystemInfo("Environment", "Production");
-		Reporter.setTestRunnerOutput("Test Execution Cucumber Report");
-	}
-*/		
+	
 	
 	
 }
---
+++ target/cucumber/report.js
@@ -0,0 +1,215 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/main/resources/Features/ContactCreation.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Check contact creation",
+  "description": "",
+  "id": "check-contact-creation",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "line": 3,
+  "name": "Check contact creationx",
+  "description": "",
+  "id": "check-contact-creation;check-contact-creationx",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "comments": [
+    {
+      "line": 4,
+      "value": "#Scenario: Check contact creationx"
+    }
+  ],
+  "line": 6,
+  "name": "I am on the login page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 7,
+  "name": "I enter details",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "I see the logged in page",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "I select the contact link",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "select the New button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "user enters first name \"\u003cfirstname\u003e\" and last name \"\u003clastname\u003e\" and middle name \"\u003cmiddlename\u003e\" and company name \"\u003ccompanyname\u003e\"",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "select save button",
+  "keyword": "And "
+});
+formatter.examples({
+  "comments": [
+    {
+      "line": 13,
+      "value": "#Then the contact is saved successfully"
+    }
+  ],
+  "line": 15,
+  "name": "",
+  "description": "",
+  "id": "check-contact-creation;check-contact-creationx;",
+  "rows": [
+    {
+      "cells": [
+        "firstname",
+        "lastname",
+        "middlename",
+        "companyname"
+      ],
+      "line": 16,
+      "id": "check-contact-creation;check-contact-creationx;;1"
+    },
+    {
+      "cells": [
+        "adam",
+        "ant",
+        "middle_name",
+        "adam_company"
+      ],
+      "line": 17,
+      "id": "check-contact-creation;check-contact-creationx;;2"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.scenario({
+  "line": 17,
+  "name": "Check contact creationx",
+  "description": "",
+  "id": "check-contact-creation;check-contact-creationx;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "comments": [
+    {
+      "line": 4,
+      "value": "#Scenario: Check contact creationx"
+    }
+  ],
+  "line": 6,
+  "name": "I am on the login page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 7,
+  "name": "I enter details",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "I see the logged in page",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "I select the contact link",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "select the New button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "user enters first name \"adam\" and last name \"ant\" and middle name \"middle_name\" and company name \"adam_company\"",
+  "matchedColumns": [
+    0,
+    1,
+    2,
+    3
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "select save button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "CreateContactStepSpec.i_am_on_the_login_page()"
+});
+formatter.result({
+  "duration": 4177263650,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CreateContactStepSpec.i_enter_details()"
+});
+formatter.result({
+  "duration": 481607515,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CreateContactStepSpec.i_see_the_logged_in_page()"
+});
+formatter.result({
+  "duration": 1095165083,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CreateContactStepSpec.i_select_the_contact_link()"
+});
+formatter.result({
+  "duration": 217734292,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CreateContactStepSpec.select_the_New_button()"
+});
+formatter.result({
+  "duration": 288246827,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "adam",
+      "offset": 24
+    },
+    {
+      "val": "ant",
+      "offset": 45
+    },
+    {
+      "val": "middle_name",
+      "offset": 67
+    },
+    {
+      "val": "adam_company",
+      "offset": 98
+    }
+  ],
+  "location": "CreateContactStepSpec.user_enters(String,String,String,String)"
+});
+formatter.result({
+  "duration": 1932953253,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CreateContactStepSpec.select_save_button()"
+});
+formatter.result({
+  "duration": 200641673,
+  "status": "passed"
+});
+});
